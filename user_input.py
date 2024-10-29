f44fdfec61039a1b31db2c1eea1bad5d05cbbbb1
Szarewa <sfarisizarewa@gmail.com>
2024-10-29 19:57:54 +0000

    Python week 1 assignment

---
+++ user_input.py
@@ -0,0 +1,10 @@
+
+name = input("Enter your name...")
+
+age = input("Enter your Age...")
+
+location = input("What's your location...")
+
+message = f"Hello {name}! How's your day. I noticed that your age is {age} and your are live from {location}. Have a great day a head."
+
+print(message)
