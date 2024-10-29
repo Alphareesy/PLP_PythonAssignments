65e5da292c50865413d7770da3dcd6a78ded47d7
Szarewa <sfarisizarewa@gmail.com>
2024-10-29 20:04:31 +0000

    Edited

--- user_input.py
+++ user_input.py
@@ -5,6 +5,6 @@ age = input("Enter your Age...")
 
 location = input("What's your location...")
 
-message = f"Hello {name}! How's your day. I noticed that your age is {age} and your are live from {location}. Have a great day a head."
+message = f"Hello {name}! How's your day. I noticed that your age is {age} and you are live from {location}. Have a nice day a head."
 
 print(message)
